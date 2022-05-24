c5a857e91124bae02933345739f9bdefa3828a4c
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-24 14:58:01 -0400

    GETindex POSTcreate built

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,2 +1,18 @@
 class SpicesController < ApplicationController
+
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(new_spice_params)
+        render json: spice, status: 201
+    end
+
+    private
+    def new_spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
 end
